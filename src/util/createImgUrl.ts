a0f1f99faae251cbcc3d29a04bedb29e44dd2a45
bruno <brunoqueiroz612@gmail.com>
2023-04-04 16:10:16 -0300

    Change image url path

--- src/util/createImgUrl.ts
+++ src/util/createImgUrl.ts
@@ -1,4 +1,4 @@
-const apiUrl = "http://localhost:3000/";
+const apiUrl = "https://tier-list-api.onrender.com/";
 
 export const createImgUrl = (imgPath: string) => {
   return apiUrl + imgPath.replace("images/", "");
