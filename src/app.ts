aec3c4481c8289b8880617f2b647ccc3482c639a
bruno <brunoqueiroz612@gmail.com>
2023-04-06 15:30:08 -0300

    Remove useless middlewares

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,8 @@ const corsOptions = {
 const app = express();
 connectToDB();
 
-app.use(express.static("images"));
+app.use(express.json());
 app.use(cors(corsOptions));
-app.use(express.json({ limit: "16mb" }));
-app.use(express.urlencoded({ limit: "16mb", extended: true }));
 app.use(proxy());
 
 app.post("/create-tier-list", cpUpload, (req, res) => {
