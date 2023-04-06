2eb48da4c9421cbd29b2981871087955ca13605a
bruno <brunoqueiroz612@gmail.com>
2023-04-06 16:28:37 -0300

    Add updateTierListItems controller function

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { connectToDB } from "./db/access";
-import { TierList } from "./db/schema";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { createTierList } from "./controllers/createTierList";
 import { getTierLists } from "./controllers/getTierLists";
 import { getSpecificTierList } from "./controllers/getSpecificTierList";
 import { updateTierList } from "./controllers/updateTierList";
-dotenv.config();
+import { updateTierListItems } from "./controllers/updateTierListItems";
 const proxy = require("html2canvas-proxy");
+dotenv.config();
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -26,14 +26,7 @@ app.post("/create-tier-list", createTierList);
 app.get("/get-tier-lists", getTierLists);
 app.get("/get-tier-list/:tierListId", getSpecificTierList);
 app.patch("/update-tier-list/:tierListId", updateTierList);
-
-app.patch("/update-tier-list-items/:tierListId", async (req, res) => {
-  const { tierListId } = req.params;
-
-  await TierList.findByIdAndUpdate(tierListId, { tierListItems: req.body });
-
-  res.json({ msg: "updated" });
-});
+app.patch("/update-tier-list-items/:tierListId", updateTierListItems);
 
 mongoose.connection.on("connected", () => {
   app.listen(3000, () => {
