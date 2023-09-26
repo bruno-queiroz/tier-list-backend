78ee4a0910066277c5fb382e87a0e26188268b10
bruno <brunoqueiroz612@gmail.com>
2023-09-26 11:48:54 -0300

    Add try and catch to catch edge cases

--- src/controllers/getTierLists.ts
+++ src/controllers/getTierLists.ts
@@ -2,7 +2,12 @@ import { Request, Response } from "express";
 import { TierList } from "../db/schema";
 
 export const getTierLists = async (req: Request, res: Response) => {
-  const allTierLists = await TierList.find();
+  try {
+    const allTierLists = await TierList.find();
 
-  res.json(allTierLists);
+    res.json({ data: allTierLists, msg: "All Tier Lists", isOk: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: "Something went wrong", isOk: false });
+  }
 };
