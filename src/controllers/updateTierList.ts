84d3c563ec8e2f8a3f91fc470dba574773ef4413
bruno <brunoqueiroz612@gmail.com>
2023-09-29 11:00:36 -0300

    Check Tier List before update

--- src/controllers/updateTierList.ts
+++ src/controllers/updateTierList.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from "express";
 import { TierList } from "../db/schema";
+import { UpdateTierListSchema } from "./schemas/updateTierListSchema";
 
 export const updateTierList = async (req: Request, res: Response) => {
-  const { tierListId } = req.params;
+  try {
+    const { tierListId } = req.params;
 
-  await TierList.findByIdAndUpdate(tierListId, {
-    tierList: JSON.stringify(req.body),
-  });
+    const tierListValidated = UpdateTierListSchema.parse(req.body);
 
-  res.json({ msg: "updated" });
+    await TierList.findByIdAndUpdate(tierListId, tierListValidated);
+
+    res.json({ data: tierListValidated, msg: "Tier List updated", isOk: true });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      msg: "Something went wrong updating your Tier List",
+      isOk: false,
+    });
+  }
 };
