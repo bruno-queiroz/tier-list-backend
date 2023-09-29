7049f5290795735242642bfd4edc74670e946c47
bruno <brunoqueiroz612@gmail.com>
2023-09-29 11:01:46 -0300

    Fix Tier List schema

--- src/controllers/schemas/createTierListSchema.ts
+++ src/controllers/schemas/createTierListSchema.ts
@@ -2,7 +2,23 @@ import { z } from "zod";
 
 export const CreateTierListSchema = z.object({
   tierListName: z.string().min(1),
+  tierList: z.array(
+    z.object({
+      color: z.string().min(1),
+      text: z.string(),
+      tierListSelectedItems: z.array(
+        z.object({
+          src: z.string(),
+          opacity: z.string().optional(),
+        })
+      ),
+    })
+  ),
+  tierListItems: z.array(
+    z.object({
+      src: z.string(),
+      opacity: z.string().optional(),
+    })
+  ),
   tierListImage: z.string().min(1),
-  tierList: z.string().min(1),
-  tierListItems: z.array(z.string()),
 });
