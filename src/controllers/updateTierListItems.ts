fac344972dff28f7c8b5f757b60cf467a2ee4956
bruno <brunoqueiroz612@gmail.com>
2023-10-02 10:59:34 -0300

    Add try and catch and validation

--- src/controllers/updateTierListItems.ts
+++ src/controllers/updateTierListItems.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from "express";
 import { TierList } from "../db/schema";
+import { UpdateTierListItemsSchema } from "./schemas/updateTierListItemsSchema";
 
 export const updateTierListItems = async (req: Request, res: Response) => {
-  const { tierListId } = req.params;
+  try {
+    const { tierListId } = req.params;
 
-  await TierList.findByIdAndUpdate(tierListId, { tierListItems: req.body });
+    UpdateTierListItemsSchema.parse(req.body);
 
-  res.json({ msg: "updated" });
+    await TierList.findByIdAndUpdate(tierListId, { tierListItems: req.body });
+
+    res.status(200).json({ msg: "Tier List Items updated", isOk: true });
+  } catch (err) {
+    console.error(err);
+
+    res.status(400).json({
+      msg: "Something went wrong updating Tier List Items",
+      isOk: false,
+    });
+  }
 };
