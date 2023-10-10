39b2736b482ac7d7b0df60d25a7c96aec759db9e
bruno <brunoqueiroz612@gmail.com>
2023-10-10 09:50:17 -0300

    Add cors options

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import { connectToDB } from "./db/access";
 import dotenv from "dotenv";
 import { createTierList } from "./controllers/createTierList";
@@ -7,26 +7,10 @@ import { getTierLists } from "./controllers/getTierLists";
 import { getSpecificTierList } from "./controllers/getSpecificTierList";
 import { updateTierList } from "./controllers/updateTierList";
 import { updateTierListItems } from "./controllers/updateTierListItems";
+import { corsOptions } from "./cors/options";
+
 const proxy = require("html2canvas-proxy");
 dotenv.config();
-
-const allowedUrls = [
-  "http://localhost:5173",
-  "https://tier-list-rho.vercel.app",
-  "https://tier-list-creator-bruno-queiroz.vercel.app",
-];
-
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    callback(null, true);
-
-    // if (allowedUrls.indexOf(origin || "") !== -1) {
-    //   callback(null, true);
-    // } else {
-    //   callback(new Error("Not Allowed by CORS"));
-    // }
-  },
-};
 
 const app = express();
 connectToDB();
