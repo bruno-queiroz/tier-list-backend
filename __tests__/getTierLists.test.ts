63bd0b81cf0619dd947ad3ec0dd9ca4a8decde7d
bruno <brunoqueiroz612@gmail.com>
2023-10-13 09:38:17 -0300

    Create tests to getTierLists

---
+++ __tests__/getTierLists.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import { jest } from "@jest/globals";
+import { TierList } from "../src/db/schema";
+import { mocked } from "jest-mock";
+
+import makeApp from "../src/app";
+
+jest.mock("../src/db/schema");
+
+const app = makeApp(() => console.log("mock database"));
+
+describe("Test app.ts", () => {
+  it("GET to /tier-list should be successful", async () => {
+    mocked(TierList).find.mockResolvedValue([{ tierList: [] }]);
+
+    const res = await request(app).get("/tier-list");
+
+    expect(res.body.data).toMatchObject([{ tierList: [] }]);
+    expect(res.status).toBe(200);
+  });
+  it("GET to /tier-list should not be successful", async () => {
+    mocked(TierList).find.mockRejectedValue([
+      { msg: "failed to get tierlists" },
+    ]);
+
+    const res = await request(app).get("/tier-list");
+
+    expect(res.status).toBe(500);
+  });
+});
