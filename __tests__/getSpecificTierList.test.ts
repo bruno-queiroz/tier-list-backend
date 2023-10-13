9dee28a994c3946bd07ef5c1998d5665bf887cc6
bruno <brunoqueiroz612@gmail.com>
2023-10-13 10:20:30 -0300

    Create tests for getSpecificTierList function

---
+++ __tests__/getSpecificTierList.test.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import { jest } from "@jest/globals";
+import { TierList } from "../src/db/schema";
+import { mocked } from "jest-mock";
+
+import makeApp from "../src/app";
+
+jest.mock("../src/db/schema");
+
+const app = makeApp(() => console.log("mock database"));
+
+describe("Test app.ts", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("GET to /tier-list/:tierListId should be successful", async () => {
+    const mockedTierList = mocked(TierList).findById.mockResolvedValue([
+      { tierList: [] },
+    ]);
+    const res = await request(app).get(`/tier-list/${123}`);
+
+    expect(mockedTierList.mock.calls).toHaveLength(1);
+    expect(mockedTierList.mock.calls[0]).toMatchObject([{ _id: "123" }]);
+    expect(res.body.data).toMatchObject([{ tierList: [] }]);
+    expect(res.status).toBe(200);
+  });
+  it("GET to /tier-list/:tierListId should not be successful", async () => {
+    const mockedTierList = mocked(TierList).findById.mockRejectedValue([
+      { msg: "failed to get tierlist" },
+    ]);
+
+    const res = await request(app).get(`/tier-list/${567}`);
+
+    expect(mockedTierList.mock.calls).toHaveLength(1);
+    expect(mockedTierList.mock.calls[0]).toMatchObject([{ _id: "567" }]);
+    expect(res.status).toBe(400);
+  });
+});
