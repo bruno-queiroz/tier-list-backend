653be250130bcb82b92a0968fa16f98c7e0f085a
bruno <brunoqueiroz612@gmail.com>
2023-12-08 15:43:04 -0300

    Create tierList mock data

---
+++ __tests__/mocks/tierListData.ts
@@ -0,0 +1,8 @@
+export const tierListData = {
+  tierList: [
+    { color: "#fff", text: "A", tierListSelectedItems: [{ src: "image" }] },
+  ],
+  tierListName: "test tier list",
+  tierListItems: [{ src: "image" }],
+  tierListImage: "image",
+};
